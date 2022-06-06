4c14b3092314d04c4cb9f42dd157c3a77a7d8630
CongSon01 <duongcongson01@gmail.com>
2022-06-06 14:55:54 +0700

    edit convert

--- Ryu_api/src/ryu-app/zookeeper.py
+++ Ryu_api/src/ryu-app/zookeeper.py
@@ -6,7 +6,10 @@ time_start = time.time()
 while True:
     time_current = time.time()
     if time_current - time_start > 3:
-        data = requests.get('http://0.0.0.0:8080/link_quality')
-        requests.post('http://10.20.0.209:5000/write_data_ryu/', data=data.text)
+        try:
+            data = requests.get('http://0.0.0.0:8080/link_quality')
+            requests.post('http://10.20.0.209:5000/write_data_ryu/', data=data.text)
+        except: 
+            print("CHUA BAT FLASK")
         time_start = time_current
     
